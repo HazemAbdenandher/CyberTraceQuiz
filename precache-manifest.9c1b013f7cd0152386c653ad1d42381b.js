a636c523e1aa73421397b72543c3373d18bfe1cc
Hazem <67316586+HazemAbdenandher@users.noreply.github.com>
2021-12-20 21:16:41 +0100

    Updates

---
+++ precache-manifest.9c1b013f7cd0152386c653ad1d42381b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8b101bb5a63606645e992f7ac419493",
+    "url": "/CyberTraceQuiz/index.html"
+  },
+  {
+    "revision": "ebfa619c350fe057d0b1",
+    "url": "/CyberTraceQuiz/static/css/main.2093f41b.chunk.css"
+  },
+  {
+    "revision": "63909c33de8d448a3bde",
+    "url": "/CyberTraceQuiz/static/js/2.ad85345e.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/CyberTraceQuiz/static/js/2.ad85345e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebfa619c350fe057d0b1",
+    "url": "/CyberTraceQuiz/static/js/main.71bb7d47.chunk.js"
+  },
+  {
+    "revision": "9367c5189fec105e7ce3",
+    "url": "/CyberTraceQuiz/static/js/runtime-main.d6bceb6c.js"
+  },
+  {
+    "revision": "35b27375c9e47b317495939c38aea7d1",
+    "url": "/CyberTraceQuiz/static/media/crown.35b27375.png"
+  },
+  {
+    "revision": "39e41ceac8310561d8ba0a413932d77d",
+    "url": "/CyberTraceQuiz/static/media/logo.39e41cea.png"
+  }
+]);
